03ad2d7250ed8c3c098748292abe4a6ef0dff40b
biglinker <danistaub@hotmail.com>
2017-03-02 23:30:36 +0100

    Anpassung XML Export
    XML Export realisert aktuell noch Tabelle Nachfrage

--- xml_export.php
+++ xml_export.php
@@ -63,9 +63,68 @@ include("login_req.php");
 			<div class="col-md-12">
 
 
-			
-			
-			
+			<?php
+//Create file name to save
+$filename = "export_xml_".date("Y-m-d_H-i",time()).".xml";
+
+
+$mysql = new Mysqli('localhost', 'agricola', 'IBZ@Agri@2017@Web', 'Agricola');
+if ($mysql->connect_errno) {
+    throw new Exception(sprintf("Mysqli: (%d): %s", $mysql->connect_errno, $mysql->connect_error));
+}
+
+//Extract data to export to XML
+$sqlQuery = 'SELECT * FROM Nachfrage';
+if (!$result = $mysql->query($sqlQuery)) {
+    throw new Exception(sprintf('Mysqli: (%d): %s', $mysql->errno, $mysql->error));
+}
+
+//Create new document 
+$dom = new DOMDocument;
+$dom->preserveWhiteSpace = FALSE;
+
+//add table in document 
+$table = $dom->appendChild($dom->createElement('table'));
+
+//add row in document 
+foreach($result as $row) {
+    $data = $dom->createElement('row');
+    $table->appendChild($data);
+
+    //add column in document 
+    foreach($row as $name => $value) {
+
+        $col = $dom->createElement('column', $value);
+        $data->appendChild($col);
+        $colattribute = $dom->createAttribute('name');
+        // Value for the created attribute
+        $colattribute->value = $name;
+        $col->appendChild($colattribute);           
+    }
+}
+
+/*
+** insert more nodes
+*/
+
+$dom->formatOutput = true; // set the formatOutput attribute of domDocument to true 
+// save XML as string or file 
+$test1 = $dom->saveXML(); // put string in test1
+$dom->save($filename); // save as file
+$dom->save('xml/'.$filename); 
+
+//shortlink
+$link="http://localhost/agricola/xml/$filename";
+
+//Output with Link
+echo "<br> <h2> Die Daten wurden als XML nach <br><a href=\"$link\"> $filename </a> <br> exportiert</h2>";
+echo "<a href=\"$link\" type=\"button\" role=\"button\" class=\"btn btn-success\">Download XML File</a> "
+
+
+
+?>
+
+					
 			</div>
 		</div>
 	</div>
