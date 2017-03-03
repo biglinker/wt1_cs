21891f223e8d3899a18a024d9b798d8a21f86141
scheph <ps@582.ch>
2017-03-03 12:07:09 +0100

    login.php überarbeitet

---
+++ footer.php
@@ -0,0 +1,5 @@
+	<footer>
+		<div class="container">
+			<h5>@2017 - IBZ Agricola-Trade developed by Philipp Schelbert, Daniel Staub</h5>
+		</div>
+	</footer>
--- login.php
+++ login.php
@@ -1,13 +1,12 @@
 ﻿ <!DOCTYPE html>
 <html lang="de">
 <?php 
-include 'session_mgmt.php';
+include "session_mgmt.php";
 include "db_connection.php";
 
 //Only for development
 ini_set("display_errors", 1);
 error_reporting(E_ALL & ~E_NOTICE);
-
 
 if(isset($_POST['B_email'])) {
 	$error = false;
@@ -16,118 +15,69 @@ if(isset($_POST['B_email'])) {
 	$passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
   
 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+		$errormessage = 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
 		$error = true;
 	} 
  
 	if(!$error) {
 
-		
 		$sql = "SELECT * FROM Benutzer WHERE B_email = '$email'"; // AND WHERE B_Password ='$passwort_hash'";
 		$result = mysqli_query($conn, $sql);
 		if(mysqli_num_rows($result) == 1) {
 			while($row = mysqli_fetch_assoc($result)) {
-				var_dump($row);
-				echo "<br>";
-				
-				echo "HALT!". $row['B_password'];
 				$password_db = $row['B_password'];
 				$user_id = $row['B_id'];
 			}
 		} 
 		else
 		{
-			echo 'Fehler beim Login, Konto nicht vorhanden!<br>';
+			$errormessage = 'Fehler beim Login, Konto nicht vorhanden!<br>';
 			$error = true;			
 		}
-		echo "<p>Passwort aus DB" . $password_db . "</p>";
-		echo "<p>Passwort aus Eingabe" . $passwort_hash . "</p>";
 		
 		//Passwort prüfen
 		IF( password_verify($passwort, $password_db) ) {
-			echo "<p>Das Passwort scheint gültig zu sein</p>";
 			//Wenn Passwort gültig, Session Variable setzen
 			
 			$_SESSION['B_id'] = $user_id;
 			$_SESSION['time'] = time();
 			
+			$successMessage = "Login erfolgreich.";
+			
 			IF( isset($_GET['fwd']) ) {
 				$location_fwd = $_GET['fwd'];
-				echo "<meta http-equiv='refresh' content='0; url=$location_fwd'>";
-				//header("Location: $location_fwd");
-				exit();
+				//Umleitung bei erfolgreichem Login
+				echo "<meta http-equiv='refresh' content='1; url=$location_fwd'>";
+				//exit();
 				
 			}
 			else
 			{
 				//Wenn kein Weiterleitungsparameter mitgeben wurde, auf Hauptseite weiterleiten
-				echo "<meta http-equiv='refresh' content='0; url=main.php'>";
-				//header("Location: $location_fwd");
-				exit();
+				echo "<meta http-equiv='refresh' content='1; url=main.php'>";
+				//exit();
 			}
-			
 			
 		}
 		else
 		{
 			//Falsches Passwort
-			echo "<p>Passworteingabe ungültig</p>";
-			
-		}
-		
-		
-		//echo $sql;
-		//var_dump($result);
-
-		//die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
-		
-		
+			$errorMessage = "Passworteingabe ungültig";
+		}		
+	
 	} 
- 
 	else {
 		$errorMessage = "E-Mail ist ungültig<br>";
 	}
  
-			
-// $statement = $pdo->prepare("SELECT * FROM users WHERE B_email = :B_email");
-// $result = $statement->execute(array('B_email' => $email));
-// $user = $statement->fetch();
- 
-//Überprüfung des Passworts
-//if ($user !== false && password_verify($passwort, $user['B_passwort'])) {
-//$_SESSION['userid'] = $user['B_id'];
-// die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
-// } else 
-//{
-	 
   
 	//Aktuell werden nur die 2 Felder Email und Passwort Hash in die Datenbank gespeichert
-	
- 
-	//Ausgabe während der Entwicklungsphase für Fehlersuche
-	echo "<p>SQL Fehler anzeigen</p>";
-	echo mysqli_error($conn);
-	echo "<br><br>"; 
-	 
-	echo "<p>Variable db SQL</p>";
-	var_dump($sql);
-	echo "<br><br>";
-	 
-	echo "<p>Variable db Result</p>";
-	var_dump($result);
-	echo "<br><br>";
-	 
-	echo "<p>Variable db Statement</p>";
-	var_dump($statement);
-	echo "<br><br>";
-	 
-	//-------------
-	 
+		 
 	if($result) { 
-		echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+		$successMessage = 'Du wurdest erfolgreich angemeldet.';
 	} 
 	else {
-			echo 'Beim Anmelden ist leider ein Fehler aufgetreten<br>';
+			$errorMessage = 'Beim Anmelden ist leider ein Fehler aufgetreten<br>';
 	}
 	
 }
@@ -153,54 +103,76 @@ if(isset($errorMessage)) {
 	<div class="container">
 		<?php include("navigation.php"); ?>
 	</div>
- 
- <!-- Page Content -->
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-12">
-			<h1>Login</h1>
-		
-		
-		<!-- HTML Code für Login -->
-				
-		
-			<img src="src/png/logo.png" alt="Agricola-Trade" class="logo">
 	
-			<form action="login.php<?php IF(isset($_GET['fwd'])) { echo "?fwd=".$_GET['fwd']; }
-			  ?>
-			" method="post" class="form-signin">
-				<h2 class="form-signin-heading"> Login </h2><br>
-				E-Mail:<br>
-				<input type="email" class="form-control" size="40" maxlength="250" name="B_email" ><br><br>
-			 
-				Dein Passwort:<br>
-				<input type="password" class="form-control" size="40"  maxlength="250" name="B_passwort" ><br>
-			 
-				<input type="submit" value="Login" class="btn btn-lg btn-success btn-block">
-				
-				<a href="pwreset.php" role="button" class="btn btn-lg btn-block btn-info">Passwort vergessen</a>
-			</form> 
-			
-			<center><h2>
-			<a href="register.php"><span class="glyphicon glyphicon-user"></span></a>  
-			<a href="register.php">Noch kein Login?</a> </h2></center>
-			
-		<!-- HTML Code für Login  -->
-			 
+	<!-- Page Header -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Login</h1>
+			</div>
+		</div>
 		
+		<!-- Fehler Ausgabe -->
+		<?php 
+			if(isset($errorMessage)) {
+		?>	
+		<div class="alert alert-danger">
+			<strong>Fehler! </strong><?php echo $errorMessage; ?>
+		</div>
+		<?php		} 	?>	
+		
+		<!-- Erfolgsmeldung -->
+		<?php 
+			if(isset($successMessage)) {
+		?>	
+		<div class="alert alert-success">
+			<strong>Erfolg! </strong><?php echo $successMessage; ?>
+		</div>
+		<?php		} 	?>	
+	
+	</div>
+	
+<!-- Page Content NEW -->
+    <div class="container">		
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+					
+			<p><img src="src/png/logo.png" alt="Agricola-Trade" class="logo"><br><br></p>
+					
+				<form class="form-horizontal" action="login.php<?php IF(isset($_GET['fwd'])) { echo "?fwd=".$_GET['fwd']; }  ?>" method="post">
+					<div class="form-group">						
+						<label class="control-label col-sm-3" for="B_email">E-Mail</label>
+						<div class="col-sm-9">
+							<input type="email" class="form-control input-lg" size="40" maxlength="250" name="B_email" placeholder="name@domain.ch">
+						</div>
+					</div>		
+						
+					<div class="form-group">				
+						<label class="control-label col-sm-3" for="A_preis">Passwort</label>
+						<div class="col-sm-9">
+							<input type="password" class="form-control input-lg" size="40"  maxlength="250" name="B_passwort">
+						</div>
+					</div>
+															
+					<div class="form-group">	
+						<div class="col-sm-offset-3 col-sm-9">
+							<input type="submit" value="Login" class="btn btn-lg btn-success btn-block">
+							<br>
+							<a href="pwreset.php" role="button" class="btn btn-lg btn-block btn-info">Passwort vergessen</a>
+						</div>
+					</div>	
+				</form> 
+				
+				<div class="col-sm-offset-3 col-sm-9">
+					<h2><a href="register.php"><span class="glyphicon glyphicon-user"></span></a>  
+						<a href="register.php">Noch kein Login?</a></h2>
+				</div>
         </div>
       </div>
-   </div>
-
-	<footer>
-      <div class="container">
-        <p class="text-muted">Place sticky footer content here.</p>
-		<h5>2017 @ IBZ Agricola-Trade developed by Philipp Schelbert, Daniel Staub</h5>
-      </div>
-
-	</footer>
-
+   </div>	
+	
+   <?php include "footer.php"; ?>
+   
     <!-- Bootstrap core JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
