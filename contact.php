e261580161ddfce0370628b50e855cf4a390aa5b
scheph <ps@582.ch>
2017-03-16 17:37:51 +0100

    Release 20170316_A
    contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,28 @@
 ﻿<?php 
+include("session_mgmt.php");
 
+require_once "function_sendmail.php";
 
+//Parameter
+$bearbeiter = "ps@582.ch";  //Diese Mail-Adresse ist verwantwortlich für die Bearbeitung der Anfragen via Kontaktformular
+
+IF( !empty($_POST) ){
+	//Variabeln umwandeln
+	$C_email = trim(htmlentities($_POST["C_email"], ENT_QUOTES));
+	$C_name = trim(htmlentities($_POST["C_name"], ENT_QUOTES));
+	$C_nachricht = nl2br(trim(htmlentities($_POST["C_nachricht"], ENT_QUOTES)));
+	
+	$datum = date("d.m.Y H:i:s",time());
+	
+	$return = mail_att($bearbeiter, "Kontaktformular Agricola", "Das Kontaktformular auf Agricola wurde ausgefüllt<br>--------------<br>Name: $C_name <br>Email: $C_email <br>Zeit: $datum <br>---------<br>Nachricht: <br>$C_nachricht", "Automailer Agricola", "webmaster@082.ch", "$C_email", NULL);
+				
+	IF( $return == true ){
+		$successMessage = "Mail wurde verschickt!";
+	}
+	else {
+		$errorMessage = "Es gab Probleme!";
+	}
+}
 
 ?>
 
@@ -17,35 +39,81 @@
 </head> 
 
 <body>
- 
-<?php 
-if(isset($errorMessage)) {
- echo $errorMessage;
-}
-?>
- 
+  
 	<!-- Include Navigation Bar -->
 	<div class="container">
 		<?php include("navigation.php"); ?>
 	</div>
  
- <!-- Page Content -->
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-12">
-			<h1>Kontakt</h1>
+	<!-- Page Header -->
+	<div class="container">
+	
+		<!-- Fehler Ausgabe -->
+		<?php 
+			if(isset($errorMessage)) {
+		?>	
+		<div class="alert alert-danger">
+			<strong>Fehler! </strong><?php echo $errorMessage; ?>
+		</div>
+		<?php		} 	?>	
 		
-				<p>Kontakformular</p>
-				
-				<p>Vorschlag für Felder: Name / Email / Text</p>
-			
-        </div>
-      </div>
-   </div>
+		<!-- Erfolgsmeldung -->
+		<?php 
+			if(isset($successMessage)) {
+		?>	
+		<div class="alert alert-success">
+			<strong>Erfolg! </strong><?php echo $successMessage; ?>
+		</div>
+		<?php		} 	?>		
+	
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Kontakt</h1>
+				<p>Nehmen Sie mit uns über das untenstehende Kontaktformular Kontakt auf.</p>
+			</div>
+		</div>
+	</div>
+ 
+   <!-- Page Content -->
+    <div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<form class="form-horizontal" action="contact.php" method="post">
 
+					<div class="form-group">					
+						<label class="control-label col-sm-2" for="C_email">E-Mail</label>
+						<div class="col-sm-10">
+							<input type="email" class="form-control" size="40" maxlength="250" name="C_email" placeholder="E-Mail">
+						</div>
+					</div>
+
+					<div class="form-group">						
+						<label class="control-label col-sm-2" for="C_name">Firma / Name</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" size="40" maxlength="250" name="C_name" placeholder="Name">						
+						</div>
+					</div>
+					
+					<div class="form-group">						
+						<label class="control-label col-sm-2" for="C_nachricht">Nachricht</label>
+						<div class="col-sm-10">
+							<textarea class="form-control" rows="5" id="C_nachricht" name="C_nachricht"></textarea>					
+						</div>
+					</div>					
+					
+					<div class="form-group">	
+						    <div class="col-sm-offset-2 col-sm-10">
+								<button type="submit" class="btn btn-default">Abschicken</button>
+							</div>
+					</div>	
+				</form> 
+			</div>
+		</div>
+	</div>
+   
 	<!-- Footer -->
-	<?php include "footer.php"; ?>
+	<?php include "footer.php" ?>
+
 
     <!-- Bootstrap core JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
